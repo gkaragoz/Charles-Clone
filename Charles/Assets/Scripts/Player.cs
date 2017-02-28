2163de9f30d98124a4a4ddd5884de58ad9653bc9
gkaragoz <annotto3@gmail.com>
2017-03-01 01:52:44 +0300

    Add player script, look to mouse position.

---
+++ Charles/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	void Update () {
+        LookToFinger();
+	}
+
+    void LookToFinger()
+    {
+        // Get mouse position
+        Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        // Get Angle in Radians
+        float AngleRad = Mathf.Atan2(targetPosition.y - transform.position.y, 
+            targetPosition.x - transform.position.x);
+        
+        // Get Angle in Degrees
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        
+        // Rotate Object
+        transform.rotation = Quaternion.Euler(0, 0, AngleDeg - 90); //Minus 90 for the offset
+    }
+}
